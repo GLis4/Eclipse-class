b481ce65ca809e0bef91b932fc365de63b577029
elisa gonçalves <elisg1711@gmail.com>
2021-06-26 10:31:50 -0300

    added news challenges

--- primeiro-projeto/src/desafios/level1/FormaGeometrica.java
+++ primeiro-projeto/src/desafios/level1/FormaGeometrica.java
@@ -4,35 +4,35 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class FormaGeometrica {
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		PedirRaioCirculo();
-		
 	}
 	static void PedirRaioCirculo() {
 		
-		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Insira o raio do circulo: ");
 		int raioCirculo = sc.nextInt();
-		sc.close();
 		calcularAreaCirculo(raioCirculo);
 	} 
 	
 	static void calcularAreaCirculo( int raio ) {
 		double resul = 3.14 * raio*raio;
 		DecimalFormat df = new DecimalFormat("#,###.00");
-		System.out.print("Area do circulo: " + df.format(resul));
+		System.out.println("Area do circulo: " + df.format(resul));
 		PedirBaseAlturaRetangulo();
 
 	}static void PedirBaseAlturaRetangulo() {
-		Scanner s = new Scanner(System.in);
+
 		System.out.println("Insira a base do retangulo: ");
-		int baseRect = s.nextInt();
+		int baseRect = sc.nextInt();
 		System.out.print("Insira a altura do retangulo: ");
-		int alturaRect = s.nextInt();
-		s.close();
+		int alturaRect = sc.nextInt();
+		sc.close();
 		calcularAreaRetangulo( baseRect, alturaRect);
 		
 	}static void calcularAreaRetangulo( int base, int altura ) {
 		double resul = base*altura;
-		System.out.print("Area do retangulo: " + resul);	}
+		System.out.print("Area do retangulo: " + resul);	
+		}
 }
