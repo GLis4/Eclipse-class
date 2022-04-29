edb6371bc150334883d71f6812f443ce45be0f71
GLis4 <elisg1711@gmail.com>
2022-04-29 17:09:13 -0300

    tests for challenge

--- primeiroProjeto/src/aulas_assunto/example/ArrayExample.java
+++ primeiroProjeto/src/aulas_assunto/example/ArrayExample.java
@@ -7,11 +7,11 @@ public class ArrayExample {
 		int quantNotas = 3;
 		int notas[] = {24,25,26};
 		
-		System.out.println("Notas: ");
 		
 		for(int i = 0; i<quantNotas; i++) {
 			System.out.println(notas[i]);
 		}
+		
 		
 		
 	}
